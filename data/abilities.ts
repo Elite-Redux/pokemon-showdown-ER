46e6d31767ce8aaf05984e846d3a1387a11dacaf
thekewlstore <thekewlstore@gmail.com>
2024-09-14 20:51:00 -0400

    Forgot to re-enable the 30% chance for bleed on spikearmor

--- data/abilities.ts
+++ data/abilities.ts
@@ -8644,7 +8644,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		onDamagingHit(damage, target, source, move) {
 			if (!this.dex.getImmunity("bld", source)) return;
 			if (move.flags["contact"] == null) return;
-			// if (!this.randomChance(3, 10)) return;
+			if (!this.randomChance(3, 10)) return;
 			if (source.status == "bld") return;
 			this.add("-activate", target, "ability: Spike Armor");
 			source.trySetStatus('bld', target, this.dex.abilities.get("spikearmor"));
