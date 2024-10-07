23e132cbd81cc75f336d8d2b7d41df79f711356f
Nyaaaa2314 <jadiel.r25@gmail.com>
2024-10-06 19:54:27 -0500

    typo fix

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -1978,7 +1978,7 @@ export const MovesText: {[k: string]: MoveText} = {
 	diamondblade: {
 		name: "Diamond Blade",
 		desc: "Has a 10% chance to bleed the target. 10% chance to set stealth rocks. Keen Edge boost.",
-	}
+	},
 	firespin: {
 		name: "Fire Spin",
 		desc: "Prevents the target from switching for four or five turns (seven turns if the user is holding Grip Claw). Causes damage to the target equal to 1/8 of its maximum HP (1/6 if the user is holding Binding Band), rounded down, at the end of each turn during effect. The target can still switch out if it is holding Shed Shell or uses Baton Pass, Flip Turn, Parting Shot, Shed Tail, Teleport, U-turn, or Volt Switch. The effect ends if either the user or the target leaves the field, or if the target uses Mortal Spin, Rapid Spin, or Substitute successfully. This effect is not stackable or reset by using this or another binding move.",
