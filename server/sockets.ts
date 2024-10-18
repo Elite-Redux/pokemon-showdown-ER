0506c95fa7c1d16d37d853e53992ba71704732eb
thekewlstore <thekewlstore@gmail.com>
2024-10-18 17:01:09 -0400

    Updating proper config file

--- server/sockets.ts
+++ server/sockets.ts
@@ -526,7 +526,7 @@ export class ServerStream extends Streams.ObjectReadWriteStream<string> {
 		this.sockets.set(socketid, socket);
 
 		let socketip = socket.remoteAddress;
-		console.debug("onConnection isTrustedProxyIp", socket.remoteAddress);
+		console.debug("onConnection isTrustedProxyIp", socket.remoteAddress, this.isTrustedProxyIp(socketip));
 		console.debug(socket.headers);
 		if (this.isTrustedProxyIp(socketip)) {
 			const ips = (socket.headers["x-forwarded-for"] || "")
