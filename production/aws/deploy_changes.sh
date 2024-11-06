d3e6d0f16a705c206908505299a16cc0f88c2cb0
Mawootad <adinfinatioma@gmail.com>
2024-11-05 18:05:10 -0800

    Pull both repositories before building

--- production/aws/deploy_changes.sh
+++ production/aws/deploy_changes.sh
@@ -3,10 +3,14 @@ forever stopall
 
 cd /home/bitnami/pokemon-showdown-ER/
 git pull
-npm run build
 
 cd /home/bitnami/pokemon-showdown-client-ER/
 git pull
+
+cd /home/bitnami/pokemon-showdown-ER/
+npm run build
+
+cd /home/bitnami/pokemon-showdown-client-ER/
 npm run build
 
 cd /home/bitnami/pokemon-showdown-ER/production/aws/
