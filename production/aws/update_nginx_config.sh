d8df3dbd82d1361f81ae414973cd500fced23897
Mawootad <adinfinatioma@gmail.com>
2024-11-06 21:13:26 -0800

    Copy nginx.conf with update_nginx_config.sh

--- production/aws/update_nginx_config.sh
+++ production/aws/update_nginx_config.sh
@@ -2,4 +2,5 @@
 sudo cp default-https-server-block.conf /opt/bitnami/nginx/conf/server_blocks/
 sudo cp er-backend.conf /opt/bitnami/nginx/conf/elite-redux/
 sudo cp er-frontend.conf /opt/bitnami/nginx/conf/elite-redux/
+sudo cp nginx.conf /opt/bitnami/nginx/conf/
 sudo nginx -s reload
