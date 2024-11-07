aec59fd17435173f7150c84a893280f81d54ac79
thekewlstore <thekewlstore@gmail.com>
2024-11-07 00:26:08 -0500

    Updating frontend build step to include the proper aws config file

--- production/aws/deploy_changes.sh
+++ production/aws/deploy_changes.sh
@@ -11,6 +11,7 @@ cd /home/bitnami/pokemon-showdown-ER/
 npm run build
 
 cd /home/bitnami/pokemon-showdown-client-ER/
+cp config/config-aws.js config/config-example.js
 npm run build
 
 cd /home/bitnami/pokemon-showdown-ER/production/aws/
