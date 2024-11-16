d6a3f2e3eb9e3ee42d8f9c4f34ee05bda0efa658
Mawootad <adinfinatioma@gmail.com>
2024-11-16 00:33:34 -0800

    Axe Kick type

--- data/moves.ts
+++ data/moves.ts
@@ -946,7 +946,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 			volatileStatus: 'confusion',
 		},
 		target: "normal",
-		type: "Fighting",
+		type: "Dark",
 	},
 	babydolleyes: {
 		num: 608,
